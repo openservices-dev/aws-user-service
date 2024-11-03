d862e42b4a786d5d37455d39cbda4b28e8bfd7d1
Matej Jellus <juffalow@juffalow.com>
2024-11-03 16:34:14 +0100

    Fix wrong error import

--- src/routes/delete.ts
+++ src/routes/delete.ts
@@ -1,10 +1,10 @@
 import express from 'express';
-import { BaseError } from '../utils/errors';
+import APIError from '../errors/APIError';
 
 const router = express.Router();
 
 router.delete('/:id', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  throw new BaseError({ message: 'Unsupported operation!', code: 400 });
+  throw new APIError({ message: 'Unsupported operation!', code: 400 });
 });
 
 export default router;
